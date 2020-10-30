9d4ff3aa6c2d88d50fc3833c6cbc50424158d3a8
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2020-10-30 10:00:57 +0100

    Moved Api and Storage to site.js

--- src/Dashboard/wwwroot/js/site.js
+++ src/Dashboard/wwwroot/js/site.js
@@ -1,9 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const $ = (selector) => {
+    const result = document.querySelectorAll(selector);
+    if (result.length === 0) return null;
+    if (result.length === 1) return result[0];
+    return Array.from(result);
+}
 
-// Write your JavaScript code.
 
-Site = (() => {
+const Site = (() => {
     return {
         resetEvents: () => {
             var xhr = new XMLHttpRequest();
@@ -24,3 +27,65 @@ Site = (() => {
         }
     }
 })();
+
+
+const Api = (() => {
+
+    return {
+        call: (method, url, callbacks) => {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState !== 4) return;
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    if (callbacks && callbacks.ok) callbacks.ok(xhr);
+                } else {
+                    if (callbacks && callbacks.error) callbacks.error(xhr);
+                }
+                if (callbacks && callbacks.always) callbacks.always(xhr);
+            };
+            xhr.open(method, url);
+            xhr.send();
+        }
+    }
+})();
+
+
+const Storage = (() => {
+
+    const storageKey = "testContexts";
+
+    const getTests = () => {
+        const testContextsString = sessionStorage.getItem(storageKey);
+        if (!testContextsString) return null;
+        return JSON.parse(testContextsString);
+    }
+    const storeTests = (testContexts) => {
+        sessionStorage.setItem(storageKey, JSON.stringify(testContexts));
+    }
+    const addTest = (testId, endPollingAt) => {
+        let testContexts = getTests();
+        if (!testContexts) testContexts = [];
+        testContexts.push({ testId: testId, endPollingAt: endPollingAt });
+        storeTests(testContexts);
+    }
+    const removeTest = (testId) => {
+        const testContexts = getTests();
+        if (!testContexts) return;
+        let spliceAt = null;
+        testContexts.forEach((test, index) => {
+            if (test.testId === testId) spliceAt = index;
+        });
+        if (spliceAt != null) {
+            testContexts.splice(spliceAt, 1);
+            storeTests(testContexts);
+        }
+    }
+
+    return {
+        getTests: getTests,
+        storeTests: storeTests,
+        addTest: addTest,
+        removeTest: removeTest
+    }
+})();
+
