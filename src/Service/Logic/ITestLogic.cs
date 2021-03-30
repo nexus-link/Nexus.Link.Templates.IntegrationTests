341a475205d3525e4a9bc007c3402578d9203f5c
Magnus Widerberg <Magnus.Widerberg@xlent.se>
2021-03-30 13:18:08 +0200

    ITestLogic in it's own file

---
+++ src/Service/Logic/ITestLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using SharedKernel;
+
+namespace Service.Logic
+{
+    /// <summary></summary>
+    public interface ITestLogic
+    {
+        /// <summary>
+        /// Create a test without parent
+        /// </summary>
+        Task<Test> CreateRootAsync(string name);
+
+        /// <summary>
+        /// Create a test (with a parent)
+        /// </summary>
+        Task<Test> CreateAsync(string name, Test parent);
+
+        /// <summary>
+        /// Set the state of a test
+        /// </summary>
+        Task SetStateAsync(Test test, StateEnum state, string message);
+
+        /// <summary>
+        /// Save a state to storage
+        /// </summary>
+        Task UpdateAsync(Test test);
+
+        /// <summary>
+        /// Get a test by id
+        /// </summary>
+        Task<Test> GetAsync(string id);
+
+        /// <summary>
+        /// Remove all tests older than a time span
+        /// </summary>
+        Task<int> PurgeAsync(TimeSpan maxAge);
+    }
+}
--- src/Service/Logic/TestLogic.cs
+++ src/Service/Logic/TestLogic.cs
@@ -118,38 +118,4 @@ namespace Service.Logic
             test.Children.Sort((a, b) => string.CompareOrdinal(a.Name, b.Name));
         }
     }
-
-    /// <summary></summary>
-    public interface ITestLogic
-    {
-        /// <summary>
-        /// Create a test without parent
-        /// </summary>
-        Task<Test> CreateRootAsync(string name);
-
-        /// <summary>
-        /// Create a test (with a parent)
-        /// </summary>
-        Task<Test> CreateAsync(string name, Test parent);
-
-        /// <summary>
-        /// Set the state of a test
-        /// </summary>
-        Task SetStateAsync(Test test, StateEnum state, string message);
-
-        /// <summary>
-        /// Save a state to storage
-        /// </summary>
-        Task UpdateAsync(Test test);
-
-        /// <summary>
-        /// Get a test by id
-        /// </summary>
-        Task<Test> GetAsync(string id);
-
-        /// <summary>
-        /// Remove all tests older than a time span
-        /// </summary>
-        Task<int> PurgeAsync(TimeSpan maxAge);
-    }
 }
